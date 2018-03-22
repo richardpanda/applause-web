6eb48ec4948138d28b88429a2a724289a7eb4eec
Richard Ho <richardpanda1993@gmail.com>
2018-03-21 17:03:54 -0700

    Update top posts every midnight

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from jinja2 import Environment, PackageLoader
 from sanic import Sanic
 from sanic.response import html
@@ -28,21 +29,30 @@ async def index(request):
     return html(template.render(topics=medium.TOPICS))
 
 
+def secs_until_midnight(dt_now):
+    dt_tomorrow = timedelta(days=1) + dt_now
+    dt_midnight = dt_tomorrow.replace(hour=0, minute=0, second=0)
+    return (dt_midnight - dt_now).total_seconds()
+
+
 async def update_top_posts():
     global top_posts
     username = os.environ['APPLAUSE_WEB__GOOGLE_USERNAME']
     password = os.environ['APPLAUSE_WEB__GOOGLE_PASSWORD']
-    top_posts = await medium.scrape_top_posts(username, password)
-    with open(FILENAME, 'w') as f:
-        json.dump(top_posts, f)
+    while True:
+        top_posts = await medium.scrape_top_posts(username, password)
+        with open(FILENAME, 'w') as f:
+            json.dump(top_posts, f)
+        await asyncio.sleep(secs_until_midnight(datetime.now()))
+
 
 if __name__ == '__main__':
     try:
         with open(FILENAME, 'r') as f:
-            top_posts = json.load(f)
+            top_posts_json = json.load(f)
             top_posts = {
                 topic: [medium.Post(*post) for post in posts]
-                for topic, posts in top_posts.items()
+                for topic, posts in top_posts_json.items()
             }
     except IOError:
         print('top_posts.txt does not exist')
