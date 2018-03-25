0199eb8df2c9709711cedd840b6f393c0e3db6e2
Richard Ho <richardpanda1993@gmail.com>
2018-03-24 22:03:50 -0700

    Add last updated to app state

--- main.py
+++ main.py
@@ -21,19 +21,46 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 env = Environment(loader=PackageLoader('main', 'templates'))
-top_posts = {topic: [] for topic in medium.TOPICS}
+
+top_posts = {
+    'by_topic': {topic: [] for topic in medium.TOPICS},
+    'last_updated': ''
+}
 
 
 @app.route('/topic/<topic:string>')
 async def show_posts(request, topic):
     template = env.get_template('posts.html')
-    return html(template.render(posts=top_posts[topic], topic=topic))
+    return html(template.render(
+        last_updated=top_posts['last_updated'],
+        posts=top_posts['by_topic'][topic],
+        topic=topic
+    ))
 
 
 @app.route('/')
 async def index(request):
     template = env.get_template('index.html')
-    return html(template.render(topics=medium.TOPICS))
+    return html(template.render(
+        last_updated=top_posts['last_updated'],
+        topics=medium.TOPICS
+    ))
+
+
+def load_top_posts_from_file(top_posts, filename):
+    try:
+        with open(filename, 'r') as f:
+            logging.info('Loading top posts from {}'.format(
+                filename
+            ))
+            top_posts_json = json.load(f)
+            top_posts['by_topic'] = {
+                topic: [medium.Post(*post) for post in posts]
+                for topic, posts in top_posts_json['by_topic'].items()
+            }
+            top_posts['last_updated'] = top_posts_json['last_updated']
+    except IOError:
+        logging.info('{} does not exist'.format(filename))
 
 
 def secs_until_midnight(dt_now):
@@ -42,40 +69,33 @@ def secs_until_midnight(dt_now):
     return (dt_midnight - dt_now).total_seconds()
 
 
-async def update_top_posts():
-    global top_posts
-
+async def update_top_posts(top_posts):
     username = os.environ['APPLAUSE_WEB__FACEBOOK_USERNAME']
     password = os.environ['APPLAUSE_WEB__FACEBOOK_PASSWORD']
 
     while True:
         logging.info('Starting Medium scraper')
-        top_posts = await medium.scrape_top_posts(username, password)
+        top_posts['by_topic'] = await medium.scrape_top_posts(username, password)
+        top_posts['last_updated'] = datetime.now().strftime(
+            '%Y-%m-%d %H:%M:%S'
+        )
         logging.info('Finished scraping Medium posts')
+
+        logging.info('Saving top posts to {}'.format(TOP_POSTS_FILENAME))
         with open(TOP_POSTS_FILENAME, 'w') as f:
-            logging.info('Saving top posts to {}'.format(TOP_POSTS_FILENAME))
             json.dump(top_posts, f)
+
         sleep_time_in_s = secs_until_midnight(datetime.now())
         logging.info(
-            'Sleeping Medium scraper for {} seconds'.format(
+            'Waking up Medium scraper in {} seconds'.format(
                 int(sleep_time_in_s)
             )
         )
         await asyncio.sleep(secs_until_midnight(datetime.now()))
 
 
-if __name__ == '__main__':
-    try:
-        with open(TOP_POSTS_FILENAME, 'r') as f:
-            logging.info('Loading top posts from {}'.format(
-                TOP_POSTS_FILENAME))
-            top_posts_json = json.load(f)
-            top_posts = {
-                topic: [medium.Post(*post) for post in posts]
-                for topic, posts in top_posts_json.items()
-            }
-    except IOError:
-        logging.info('{} does not exist'.format(TOP_POSTS_FILENAME))
+def main():
+    load_top_posts_from_file(top_posts, TOP_POSTS_FILENAME)
 
     loop = asyncio.get_event_loop()
     server = app.create_server(
@@ -83,6 +103,10 @@ if __name__ == '__main__':
         port=int(os.getenv('APPLAUSE_WEB__SERVER_PORT', 8080))
     )
     asyncio.ensure_future(server, loop=loop)
-    loop.create_task(update_top_posts())
+    loop.create_task(update_top_posts(top_posts))
     logging.info('Starting web server and scraper')
     loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
