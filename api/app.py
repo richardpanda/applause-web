d557103e614df571e9c6ef6a09d9657107922884
Richard Ho <richardpanda1993@gmail.com>
2018-08-17 17:34:24 -0700

    Fix posts route

--- api/app.py
+++ api/app.py
@@ -1,16 +1,18 @@
 import json
 import redis
+import os
 
 from flask import Flask, jsonify
 
-r = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+r = redis.StrictRedis(host=REDIS_HOST, port=6379, db=0)
 app = Flask(__name__)
 
 
-@app.route("/api/topics/<string:topic>/posts")
+@app.route("/api/topic/<string:topic>/posts")
 def show_posts(topic):
     posts = r.lrange(f"posts:{topic}", 0, -1)
-    return jsonify(top_posts=[json.loads(post.decode()) for post in posts])
+    return jsonify(posts=[json.loads(post.decode()) for post in posts])
 
 
 @app.route("/api/topics")
